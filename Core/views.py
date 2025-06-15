66e5b5b8f961fec8361796c3299b5b44684be60d
Nicolás Doyhenart <nidoyhe@gmail.com>
2025-06-14 21:21:30 -0300

    Primer commit con avances del proyecto

---
+++ Core/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .forms import PeliculaForm, GeneroForm, DirectorForm
+from .models import Pelicula
+
+def home(request):
+    return render(request, 'Core/home.html')
+
+def index(request):
+    peliculas = Pelicula.objects.all()
+    return render(request, 'Core/inicio.html', {'peliculas': peliculas})
+
+def agregar_pelicula(request):
+    if request.method == 'POST':
+        form = PeliculaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # Redirigí a la página principal u otra
+    else:
+        form = PeliculaForm()
+    return render(request, 'Core/agregar_pelicula.html', {'form': form})
+
+def agregar_genero(request):
+    if request.method == 'POST':
+        form = GeneroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = GeneroForm()
+    return render(request, 'Core/agregar_genero.html', {'form': form})
+
+def agregar_director(request):
+    if request.method == 'POST':
+        form = DirectorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = DirectorForm()
+    return render(request, 'Core/agregar_director.html', {'form': form})
+
+def buscar_pelicula(request):
+    return render(request, 'Core/buscar_pelicula.html')
